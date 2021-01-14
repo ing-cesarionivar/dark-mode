91931c966b6d6d5d5b228afc34cab0b5cb929fa0
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-01-14 18:44:46 -0400

    Guardando el modo en el localStorage

--- js/main.js
+++ js/main.js
@@ -5,4 +5,23 @@ btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
 
+    // Guardamos el modo en localStorage
+    if(document.body.classList.contains('dark')) {
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
+
 });
+
+// Obtenemos el modo actual en el cual nos encontramos
+if(localStorage.getItem('dark-mode') === 'true') {
+    
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+    
+} else {
+    
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+}
